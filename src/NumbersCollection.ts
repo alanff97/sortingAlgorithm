326924489ef160ee500e5c519589fcce173c629b
alanff97 <alan.figueroa.cl@gmail.com>
2023-06-21 19:43:48 +0200

    NumbersCollection with compare method

---
+++ src/NumbersCollection.ts
@@ -0,0 +1,6 @@
+export class NumberCollection {
+  constructor(public data: number[]) {}
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+}
