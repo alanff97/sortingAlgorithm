0e449055f719cb17ec4581fd1065f49d8755dbd9
alanff97 <alan.figueroa.cl@gmail.com>
2023-06-21 20:11:34 +0200

    created interface Sorteable

---
+++ build/CharactersCollection.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CharactersCollections = void 0;
+class CharactersCollections {
+    constructor(data) {
+        this.data = data;
+    }
+}
+exports.CharactersCollections = CharactersCollections;
---
+++ src/CharactersCollection.ts
@@ -0,0 +1,5 @@
+export class CharactersCollections {
+    constructor(public data: string){
+        
+    }
+}
