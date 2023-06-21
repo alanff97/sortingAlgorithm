824a9a5ddec80322c1d7fa51ca4597c91adc5cb6
alanff97 <alan.figueroa.cl@gmail.com>
2023-06-21 19:46:13 +0200

    swap method

--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -3,4 +3,9 @@ export class NumberCollection {
   compare(leftIndex: number, rightIndex: number): boolean {
     return this.data[leftIndex] > this.data[rightIndex];
   }
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftHand = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = leftHand;
+  }
 }
