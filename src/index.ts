22f10bfc683da59f4094b323da5cdf61823f06b2
alanff97 <alan.figueroa.cl@gmail.com>
2023-06-22 20:37:19 +0200

    Sorted method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollections } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
@@ -11,3 +12,13 @@ const charactersCollection = new CharactersCollections('Xaayb');
 const sorterChar = new Sorter(charactersCollection);
 sorterChar.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(3);
+linkedList.add(4);
+
+const sorterList = new Sorter(linkedList);
+sorterList.sort();
+linkedList.print();
