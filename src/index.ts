7c249c375910dcb7d1b7deadb4a8d56a61891264
alanff97 <alan.figueroa.cl@gmail.com>
2023-06-22 23:20:56 +0200

    refact charaters and linked list

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import { CharactersCollections } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(sorter.collection);
+
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollections('Xaayb');
-const sorterChar = new Sorter(charactersCollection);
-sorterChar.sort();
+charactersCollection.sort();
+
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -18,7 +18,5 @@ linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(3);
 linkedList.add(4);
-
-const sorterList = new Sorter(linkedList);
-sorterList.sort();
+linkedList.sort();
 linkedList.print();
